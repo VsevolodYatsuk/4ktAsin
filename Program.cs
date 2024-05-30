967e271814a648aadc08650691533da0280915c4
Всеволод <130091517+VsevolodYatsuk@users.noreply.github.com>
2024-05-31 00:32:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,11 +5,9 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // Инициализация матриц A и B
         Matrix a = new Matrix(2, 3);
         Matrix b = new Matrix(3, 2);
 
-        // Заполнение матриц данными для примера
         a.Data[0, 0] = 1; a.Data[0, 1] = 2; a.Data[0, 2] = 3;
         a.Data[1, 0] = 4; a.Data[1, 1] = 5; a.Data[1, 2] = 6;
 
@@ -17,10 +15,8 @@ class Program
         b.Data[1, 0] = 9; b.Data[1, 1] = 10;
         b.Data[2, 0] = 11; b.Data[2, 1] = 12;
 
-        // Асинхронное умножение с использованием конвейера
         Matrix result = await Matrix.MultiplyPipelineAsync(a, b);
 
-        // Вывод результата
         for (int i = 0; i < result.Rows; i++)
         {
             for (int j = 0; j < result.Cols; j++)
